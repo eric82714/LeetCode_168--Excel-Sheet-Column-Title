6583d9de88b7c2c469555d80c07fd90e05c9bb66
eric82714 <0958820714ab@gmail.com>
2020-01-25 12:35:11 +0800

    Create excel_sheet_column_title.cpp

---
+++ C++/excel_sheet_column_title.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string title;
+        
+        while(n > 0) {
+            title = char('A' + ((n-1) % 26)) + title;
+            n = (n-1) / 26;
+        }
+        
+        return title;
+    }
+};
